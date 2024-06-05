9996d5e1d7c3e79f113aeb179786707317e28085
ogwerset <140664646+ogwerset@users.noreply.github.com>
2024-06-05 15:58:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 <script src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
 <script type="text/javascript">
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-		var adobeDCView = new AdobeDC.View({clientId: "77235daa884c4083b4d894813e5d325a", divId: "adobe-dc-view"});
+		var adobeDCView = new AdobeDC.View({clientId: "1593701048e640609653adf30ddb1c2d", divId: "adobe-dc-view"});
 		adobeDCView.previewFile({
 			content:{location: {url: "https://raw.githubusercontent.com/ogwerset/cv2024/main/MarchlewskiAdam-CV-042024.pdf"}},
 			metaData:{fileName: "MarchlewskiAdam-CV-062024"}
